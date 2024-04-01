85c050edee0c9e4cd56f4e67d6cdb40f5df6436b
Jesus Peralta <jperalta@codepels.de>
2024-04-01 18:58:17 +0200

    feat: search by name and exported bin files for github pages

--- src/app/people/components/people-overview/people-overview.component.ts
+++ src/app/people/components/people-overview/people-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PeopleFacadeService } from '../../state/people.facade.service';
 import { MatGridListModule} from '@angular/material/grid-list';
@@ -10,11 +10,23 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { AddPeopleComponent } from '../add-people/add-people.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-people-overview',
   standalone: true,
-  imports: [CommonModule, MatGridListModule, RouterOutlet, MatSidenavModule, MatButtonModule, MatIconModule, MatDialogModule, ReactiveFormsModule, FormsModule],
+  imports: [
+    CommonModule, 
+    MatGridListModule, 
+    RouterOutlet, 
+    MatSidenavModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    MatDialogModule,
+    ReactiveFormsModule, 
+    FormsModule,
+    MatInputModule
+  ],
   templateUrl: './people-overview.component.html',
   styleUrl: './people-overview.component.scss'
 })
@@ -22,7 +34,10 @@ export class PeopleOverviewComponent {
   private peopleFacadeService = inject(PeopleFacadeService);
   private router = inject(Router);
 
-  public peopleList = this.peopleFacadeService.peopleList;
+  private filterQuery = signal<string>('');
+  public peopleList = computed(() => {
+    return this.peopleFacadeService.peopleList().filter(x => x.name.toLowerCase().includes(this.filterQuery().toLowerCase()));
+  });
 
   constructor(public dialog: MatDialog) {}
 
@@ -38,6 +53,10 @@ export class PeopleOverviewComponent {
 
   public addPeople() {
     this.dialog.open(AddPeopleComponent);
+  }
+
+  applyFilter(value: string): void {
+    this.filterQuery.set(value)
   }
 
 }
